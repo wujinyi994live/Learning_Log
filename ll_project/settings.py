c2a0318e9988559cd10a851b641a9445b4069905
wujiyi <wujinyi994@gmail.com>
2024-06-05 15:44:49 +0800

    第一次提交，本地运行无问题

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
-
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,8 +83,7 @@ WSGI_APPLICATION = 'll_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-       # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join(os.environ.get('ZEABUR_APP_FS', ''), 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -126,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
