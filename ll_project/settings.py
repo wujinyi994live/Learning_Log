603832d02296f04e1258d9d625f8a00834b5d941
wujiyi <wujinyi994@gmail.com>
2024-06-05 15:19:01 +0800

    第一次提交，本地运行无问题

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,7 +85,8 @@ WSGI_APPLICATION = 'll_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(os.environ.get('ZEABUR_APP_FS', ''), 'db.sqlite3'),
     }
 }
 
